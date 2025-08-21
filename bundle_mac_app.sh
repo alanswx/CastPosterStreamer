886ff22556d6f94af480ee0d68c3215ce4b3640f
Alan Steremberg <alan.steremberg@gmail.com>
2025-08-21 16:13:05 -0700

    fix

--- bundle_mac_app.sh
+++ bundle_mac_app.sh
@@ -9,7 +9,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Finally, run the build command again
-python setup.py py2app --semi-standalone
+python setup.py py2app 
 
 # Sign the application
 codesign --force --verify --verbose --sign "Apple Development: Alan Steremberg (DALPRRY8F7)" "dist/Posters.app"
